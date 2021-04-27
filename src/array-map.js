c911220c89c33751025c3a3000b3fc668e993162
taylor-tokareff <taylortokareff@gmail.com>
2021-04-26 22:20:04 -0700

    2 tests pass

---
+++ src/array-map.js
@@ -0,0 +1,23 @@
+export function doubleNumbers(arr) {
+  return arr.map(item => {
+    const multiply = item * 2;
+    return multiply;
+  });
+
+}
+
+export function stringyMaJig(arr) {
+  return arr.map(item => {
+    const stringy = item + '';
+    return stringy;
+  });
+}
+
+export function theCapitolizer(arr) {
+  return arr.map(item => {
+    const firstLower = item.toLowerCase();
+    const nowUpper = item.toUpperCase[0]();
+
+    return something I dunno; __;
+  });
+}
