5aedf821dca52945a6a8514d2c6713abe164ae6d
taylor-tokareff <taylortokareff@gmail.com>
2021-05-10 13:50:46 -0700

    all tests pass

--- src/more-object-keys-entries.js
+++ src/more-object-keys-entries.js
@@ -12,3 +12,39 @@ export function changeNumbers(obj) {
   });
   return array;
 };
+
+export function totalCharacters(arr) {
+  let count = 0;
+
+  arr.forEach((item) => {
+
+    if (item.name) count++;
+
+    if (item.spouse) count++;
+
+    if (item.children) count = count + item.children.length;
+  });
+  return count;
+
+};
+
+export function sortByChildAmount(arr) {
+  const anArray = arr.sort((a, b) => {
+    return a.children.length - b.children.length;
+  })
+    .sort((a, b) => {
+      return a.name - b.name;
+    });
+  return anArray;
+};
+
+export function hasChildren(arr, character) {
+  let hasChildren = false;
+
+  arr.forEach((item) => {
+    if (item.name === character) {
+      if (item.children.length > 0) return hasChildren = true;
+    };
+  });
+  return hasChildren;
+}
